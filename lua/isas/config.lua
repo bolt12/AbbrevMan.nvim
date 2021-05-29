1f1a385aa4a73771907e7bff54b72692bc65e133
kdav5758 <kdav5758@gmail.com>
2021-05-29 18:49:33 -0500

    fixed conditionals 8

--- lua/isas/config.lua
+++ lua/isas/config.lua
@@ -26,24 +26,24 @@ function config.set_options(opts)
 		vim.cmd("echo 'Opt = "..opt.."'")
 		if (config.options[opt] ~= nil) then		-- not nil
 			vim.cmd("echo 'Got hereeeeeeeeeeeeeeeeeeeee'")
-			if (type(opts[opt]) == "table" and opt ~= "natural_dictionaries") then
+			if (type(opts[opt]) == "table") then
 			-- if (type(opts[opt]) == "table" and opt ~= "natural_dictionaries") then
 
-				vim.cmd("echo 'Was a table'")
-					for inner_opt, _ in pairs(opts[opt]) do
-						if (config.options[opt][inner_opt] ~= nil) then		-- not nil
-							config.options[opt][inner_opt] = opts[opt][inner_opt]
-						end
-					end
-
-				-- if (opt ~= "natural_dictionaries") then
-				-- 	vim.cmd("echo 'Was not natural or programming'")
+				-- vim.cmd("echo 'Was a table'")
 				-- 	for inner_opt, _ in pairs(opts[opt]) do
 				-- 		if (config.options[opt][inner_opt] ~= nil) then		-- not nil
 				-- 			config.options[opt][inner_opt] = opts[opt][inner_opt]
 				-- 		end
 				-- 	end
-				-- end
+
+				if (opt ~= "natural_dictionaries") then
+					vim.cmd("echo 'Was not natural or programming'")
+					for inner_opt, _ in pairs(opts[opt]) do
+						if (config.options[opt][inner_opt] ~= nil) then		-- not nil
+							config.options[opt][inner_opt] = opts[opt][inner_opt]
+						end
+					end
+				end
 			else
 				config.options[opt] = opts[opt]
 			end
