523a62f16f9bcad420a90d41179f9f2c7aa45a8f
kdav5758 <kdav5758@gmail.com>
2021-05-29 18:42:58 -0500

    fixed conditionals 6

--- lua/isas/config.lua
+++ lua/isas/config.lua
@@ -25,16 +25,23 @@ function config.set_options(opts)
     for opt, _ in pairs(opts) do
 		vim.cmd("echo 'Opt = "..opt.."'")
 		if (config.options[opt] ~= nil) then		-- not nil
-			if (type(opts[opt]) == "table") then
+			if (type(opts[opt]) == "table" and opt ~= "natural_dictionaries") then
+
 				vim.cmd("echo 'Was a table'")
-				if (opt ~= "natural_dictionaries") then
-					vim.cmd("echo 'Was not natural or programming'")
 					for inner_opt, _ in pairs(opts[opt]) do
 						if (config.options[opt][inner_opt] ~= nil) then		-- not nil
 							config.options[opt][inner_opt] = opts[opt][inner_opt]
 						end
 					end
-				end
+
+				-- if (opt ~= "natural_dictionaries") then
+				-- 	vim.cmd("echo 'Was not natural or programming'")
+				-- 	for inner_opt, _ in pairs(opts[opt]) do
+				-- 		if (config.options[opt][inner_opt] ~= nil) then		-- not nil
+				-- 			config.options[opt][inner_opt] = opts[opt][inner_opt]
+				-- 		end
+				-- 	end
+				-- end
 			else
 				config.options[opt] = opts[opt]
 			end
