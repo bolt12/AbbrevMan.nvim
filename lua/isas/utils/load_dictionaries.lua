ebda80e93d9773381a1cb345177ecf48f8082fee
kdav5758 <kdav5758@gmail.com>
2021-05-28 22:58:55 -0500

    uncommented load() code

--- lua/isas/utils/load_dictionaries.lua
+++ lua/isas/utils/load_dictionaries.lua
@@ -23,27 +23,26 @@ end
 function M.load()
 
 	for u_dict in pairs(user_dicts) do
-		vim.cmd("echo 'dict = "..u_dict.."'")
--- 		if has_value(isas_dicts, u_dict) then
---
--- 			local inner_isas_dict = require("isas.dictionaries."..u_dict)
---
--- 			for element in inner_isas_dict do
--- 				if has_value(user_dicts[u_dict], element) then
--- 					inner_isas_dict[element] = user_dicts[u_dict][element]
--- 					-- if not (user_dicts[u_dict][element] == "rm_isas") then
--- 					-- 	inner_isas_dict[element] = user_dicts[u_dict][element]
--- 					-- else
--- 					-- 	table.
--- 					-- end
--- 				end
--- 			end
---
--- 			for element in inner_isas_dict do
--- 				map_iabbrev(element, inner_isas_dict[element])
--- 			end
---
--- 		end
+		if has_value(isas_dicts, u_dict) then
+
+			local inner_isas_dict = require("isas.dictionaries."..u_dict)
+
+			for element in pairs(inner_isas_dict) do
+				if has_value(user_dicts[u_dict], element) then
+					inner_isas_dict[element] = user_dicts[u_dict][element]
+					-- if not (user_dicts[u_dict][element] == "rm_isas") then
+					-- 	inner_isas_dict[element] = user_dicts[u_dict][element]
+					-- else
+					-- 	table.
+					-- end
+				end
+			end
+
+			for element in pairs(inner_isas_dict) do
+				map_iabbrev(element, inner_isas_dict[element])
+			end
+
+		end
 	end
 
 end
