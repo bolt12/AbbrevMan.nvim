e231dba2fc2a3f9f9bc33538bf6a742118e54be8
kdav5758 <kdav5758@gmail.com>
2021-05-29 16:47:36 -0500

    fixed wrong require statement

--- lua/isas/completions/aa_loaded_dictionaries.lua
+++ lua/isas/completions/aa_loaded_dictionaries.lua
@@ -4,7 +4,7 @@
 local M = {}
 
 M.arguments = {}
-local loaded_dicts = require("lua.isas.utils.abbrev.modules.load_dictionaries").loaded_dicts
+local loaded_dicts = require("isas.utils.abbrev.modules.load_dictionaries").loaded_dicts
 
 function M.available_commands()
 	for dict, _ in pairs(loaded_dicts) do
