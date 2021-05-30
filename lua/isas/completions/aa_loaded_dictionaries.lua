d4e9b651c0a10bacc36d39aab103c85100ae76da
kdav5758 <kdav5758@gmail.com>
2021-05-30 16:17:20 -0500

    added func to remove element from table 2

--- lua/isas/completions/aa_loaded_dictionaries.lua
+++ lua/isas/completions/aa_loaded_dictionaries.lua
@@ -4,10 +4,12 @@
 local M = {}
 
 M.arguments = {}
-local loaded_dicts = require("isas.utils.abbrev.modules.load_dictionaries").loaded_dicts
+-- local loaded_dicts = require("isas.utils.abbrev.modules.load_dictionaries").loaded_dicts
 
 function M.available_commands()
-	for dict, dict_name in pairs(loaded_dicts) do
+
+	-- for dict, dict_name in pairs(loaded_dicts) do
+	for dict, dict_name in pairs(require("isas.utils.abbrev.modules.load_dictionaries").loaded_dicts) do
 		M.arguments[dict_name] = dict_name
 	end
 
