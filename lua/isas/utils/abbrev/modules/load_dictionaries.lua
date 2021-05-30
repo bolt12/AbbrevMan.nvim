98962258a8c85771149ee5fcc58d096f66753249
kdav5758 <kdav5758@gmail.com>
2021-05-29 22:18:31 -0500

    testing load_programming_dictionaries_at_startup 4

--- lua/isas/utils/abbrev/modules/load_dictionaries.lua
+++ lua/isas/utils/abbrev/modules/load_dictionaries.lua
@@ -121,6 +121,8 @@ function M.load_programming_dictionaries_at_startup()
 				end
 			end
 --
+			vim.cmd("echo '"..parse_iabbrev_pr(inner_isas_dict).."'")
+
 			local file_type = u_dict:gsub("pr_", "")
 -- --
 			require("isas.utils.abbrev.modules.isas_augroups").set_augroups(
