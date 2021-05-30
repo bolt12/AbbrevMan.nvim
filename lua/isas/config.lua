e0a8960081be52ea9c97130add39c72e6ec3e009
kdav5758 <kdav5758@gmail.com>
2021-05-29 23:04:26 -0500

    testing load_programming_dictionaries_at_startup 18

--- lua/isas/config.lua
+++ lua/isas/config.lua
@@ -25,7 +25,7 @@ function config.set_options(opts)
     for opt, _ in pairs(opts) do
 		if (config.options[opt] ~= nil) then		-- not nil
 			if (type(opts[opt]) == "table") then
-				if (opt ~= "natural_dictionaries" or opt ~= "programming_dictionaries") then
+				if (opt ~= "natural_dictionaries" and opt ~= "programming_dictionaries") then
 					for inner_opt, _ in pairs(opts[opt]) do
 						if (config.options[opt][inner_opt] ~= nil) then		-- not nil
 							config.options[opt][inner_opt] = opts[opt][inner_opt]
